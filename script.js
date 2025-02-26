1b4634c7590eec34c59e5a6b55e2a80d7f5e7efe
nidhigothwal <165782089+nidhigothwal@users.noreply.github.com>
2025-02-26 22:14:53 +0530

    update modal

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('js file is working')
+
+function samsungopenmodel() {
+   document.querySelector('.model-samsung').style.display = 'block';
+}
+function iphoneopenmodel() {
+   document.querySelector('.model-iphone').style.display = 'block';
+}
+
+function closeModalBox() {
+document.querySelector('.model-samsung').style.display = 'none';
+}
+function  closeiphonebox() {
+   document.querySelector('.model-iphone').style.display = 'none';
+}
